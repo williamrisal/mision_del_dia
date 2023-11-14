ac486a8f295c249aa98b2477807bf6aaab62eef8
jordan-sissilian <jordan.sissilian@protonmail.com>
2023-11-14 19:04:36 +0100

    .

--- misión-del-dia/BottomNavbar/BottomNavbarView.swift
+++ misión-del-dia/BottomNavbar/BottomNavbarView.swift
@@ -4,32 +4,32 @@ struct BottomNavbarView: View {
     @Binding var viewLoad: Int
     @State private var selectedTab = 1
 
+    var logo: [String] = ["gift.fill", "medal.fill", "person.crop.circle"]
+
     var body: some View {
-        TabView(selection: $selectedTab) {
-                NavigationView {
+        HStack(spacing: 0) {
+            Spacer().frame(width: 20)
+            ForEach(0..<3) { index in
+                Button(action: {
+                    viewLoad = index
+                }) {
+                    Image(systemName: logo[index])
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .padding(2)
                 }
-                .tabItem {
-                    Image(systemName: "tablecells")
+                .foregroundColor(viewLoad == index ? (viewLoad == 0 ? .red : viewLoad == 1 ? .green : .blue) : .gray)
+                if index < 2 {
+                    Spacer()
                 }
-                .tag(0)
-                
-                NavigationView { }
-                    .tabItem {
-                        Image(systemName: "tablecells")
-                    }
-                    .tag(1)
-                
-                NavigationView { }
-                    .tabItem {
-                        Image(systemName: "tablecells")
-                    }
-                    .tag(2)
-                    .background(Color.yellow)
             }
-        .onChange(of: selectedTab) { lastTab, newTab in
-            if (viewLoad == lastTab) {
-                viewLoad = newTab
-            }
+            Spacer().frame(width: 20)
         }
+        .frame(width: (75 / 100) * UIScreen.main.bounds.width)
+        .frame(height: (5 / 100) * UIScreen.main.bounds.height)
+        .padding()
+        .background(Color.white.opacity(0.95))
+        .cornerRadius(35)
+        .shadow(radius: 7)
     }
 }
--- misión-del-dia/ContentView.swift
+++ misión-del-dia/ContentView.swift
@@ -1,35 +1,33 @@
 // ContentView.swift
 import SwiftUI
-
 struct ContentView: View {
     @State private var viewLoad = 1
-
 
     var body: some View {
         ZStack {
             BackgroundView()
 
-            VStack {
-                VStack {
-                    switch viewLoad {
-                    case 0:
-                        Text("0")
-                    case 1:
-                        Text("1")
-                    case 2:
-                        Text("2")
-                    default:
-                        Text("1")
+             VStack {
+                Spacer()
+                ScrollView {
+                     switch viewLoad {
+                     case 0:
+                        Text("mettre la premire view")
+                     case 1:
+                        Text("mettre la deuxieme view")
+                     case 2:
+                        Text("mettre la 3 view")
+                     default:
+                        Text("mettre la 1 view")
                     }
                 }
-                .frame(maxHeight: .infinity)
-                VStack {
-                    BottomNavbarView(viewLoad: $viewLoad)
-                        .padding()
-                }
-                .frame(height: (10 / 100) * UIScreen.main.bounds.height)
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height - 50)
             }
+             .background(viewLoad == 0 ? .red : viewLoad == 1 ? .green : .blue)
+             .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+            BottomNavbarView(viewLoad: $viewLoad)
+                .position(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height / 2.2)
+                .frame(height: (10 / 100) * UIScreen.main.bounds.height)
         }
     }
 }
-
