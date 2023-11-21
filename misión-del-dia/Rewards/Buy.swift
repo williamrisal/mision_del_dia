b39f847f9566bf5ea55d610b22086fcd396a2e80
jordan-sissilian <jordan.sissilian@protonmail.com>
2023-11-21 17:15:58 +0100

    .

--- misión-del-dia/Rewards/Buy.swift
+++ misión-del-dia/Rewards/Buy.swift
@@ -38,14 +38,14 @@ struct BuyView: View {
                     Text(name)
                         .foregroundStyle(.white)
                     Spacer()
-                    HStack {
-                        Text(String(price))
-                        Image(systemName: "app.gift.fill")
-                    }
-                        foregroundColor(.white)
-                        .padding(5)
-                        .background(Int(sold) >= price ? .green.opacity(0.6) : .red.opacity(0.6))
-                        .cornerRadius(10)
+//                    HStack {
+//                        Text(String(price))
+//                        Image(systemName: "app.gift.fill")
+//                    }
+//                        foregroundColor(.white)
+//                        .padding(5)
+//                        .background(Int(sold) >= price ? .green.opacity(0.6) : .red.opacity(0.6))
+//                        .cornerRadius(10)
                     Spacer()
                 }
                 .frame(maxHeight: (75 / 100) * UIScreen.main.bounds.height)
