039e9f3c75dd7a7f31c4b4111ce9727984338e0b
William risal <william.risal@epitech.eu>
2023-11-21 22:22:27 +0100

    update square View pour lespace

--- misión-del-dia/MissionSquareView/SquareView.swift
+++ misión-del-dia/MissionSquareView/SquareView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct SquareView: View {
     var body: some View {
         VStack {
-            Spacer()
             Rectangle()
                 .fill(Color.white)
                 .overlay(
